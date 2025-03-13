7f440ebdcd7e3367e3a83ef45284c3f05a1e45da
Konfusha <konfusha@DESKTOP-TIVQ5FA>
2025-03-13 23:55:10 +0500

    Pharmacy search 2.0

--- YandexAPITools.py
+++ YandexAPITools.py
@@ -1,5 +1,9 @@
-def find_spn_delta(json_response):
+def find_spn_delta(json_response, *points_responses):
     envelope = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["boundedBy"]["Envelope"]
-    dx = float(envelope["upperCorner"].split()[0]) - float(envelope["lowerCorner"].split()[0])
-    dy = float(envelope["upperCorner"].split()[1]) - float(envelope["lowerCorner"].split()[1])
-    return [dx, dy]
+    x = [float(envelope["upperCorner"].split()[0]), float(envelope["lowerCorner"].split()[0])]
+    y = [float(envelope["upperCorner"].split()[1]), float(envelope["lowerCorner"].split()[1])]
+    for point_response in points_responses:
+        place = point_response["features"][0]
+        x += [place["properties"]["boundedBy"][1][0], place["properties"]["boundedBy"][0][0]]
+        y += [place["properties"]["boundedBy"][1][1], place["properties"]["boundedBy"][0][1]]
+    return [max(x) - min(x), max(y) - min(y)]
--- solution.py
+++ solution.py
@@ -1,11 +1,19 @@
 import sys
-from io import BytesIO  # Этот класс поможет нам сделать картинку из потока байт
+from io import BytesIO
 from YandexAPITools import find_spn_delta
 import json
-
+import os
+from dotenv import load_dotenv
 import requests
 from PIL import Image
 
+
+path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(path):
+    load_dotenv(path)
+    SEARCH_API_KEY = os.environ.get('SEARCH_API_KEY')
+    GEOCODER_API_KEY = os.environ.get('GEOCODER_API_KEY')
+    STATIC_MAP_API_KEY = os.environ.get('STATIC_MAP_API_KEY')
 # Пусть наше приложение предполагает запуск:
 # python search.py Москва, ул. Ак. Королева, 12
 # Тогда запрос к геокодеру формируется следующим образом:
@@ -14,7 +22,7 @@ toponym_to_find = " ".join(sys.argv[1:])
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
 geocoder_params = {
-    "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+    "apikey": GEOCODER_API_KEY,
     "geocode": toponym_to_find,
     "format": "json"}
 
@@ -26,21 +34,51 @@ if not response:
 
 # Преобразуем ответ в json-объект
 json_response = response.json()
-json.dump(json_response, open('request0.json', 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
 # Получаем первый топоним из ответа геокодера.
 toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
 # Координаты центра топонима:
 toponym_coodrinates = toponym["Point"]["pos"]
 # Долгота и широта:
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
 
-# Собираем параметры для запроса к StaticMapsAPI:
+search_api_server = "https://search-maps.yandex.ru/v1/"
+search_params = {
+    "apikey": SEARCH_API_KEY,
+    "text": "аптека",
+    "lang": "ru_RU",
+    "ll": toponym_longitude + ',' + toponym_lattitude,
+    "type": "biz"
+}
+ 
+response = requests.get(search_api_server, params=search_params)
+if not response:
+    #...
+    pass
+ 
+pharmacy_response = response.json()
+json.dump(pharmacy_response, open('request0.json', 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
+
+# Получаем первую найденную организацию.
+organization = pharmacy_response["features"][0]
+# Название организации.
+org_name = organization["properties"]["CompanyMetaData"]["name"]
+# Адрес организации.
+org_address = organization["properties"]["CompanyMetaData"]["address"]
+
+work_time = organization["properties"]["CompanyMetaData"]["Hours"]["text"]
+
+# Получаем координаты ответа.
+pharmacy_point = organization["geometry"]["coordinates"]
+org_point = f"{pharmacy_point[0]},{pharmacy_point[1]}"
+distance = ((float(toponym_longitude) - pharmacy_point[0]) ** 2 + (float(toponym_longitude) - pharmacy_point[0]) ** 2) ** 0.5
+print(f"Адрес: {org_address}\nНазвание: {org_name}\nВремя работы: {work_time}\nРасстояние: {distance}")
+
+
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join(map(str, find_spn_delta(json_response))),
-    "apikey": apikey,
-    "pt": "{0},pm2dgl".format(toponym_longitude + ',' + toponym_lattitude)
+    "spn": ",".join(map(str, find_spn_delta(json_response, pharmacy_response))),
+    "apikey": STATIC_MAP_API_KEY,
+    "pt": "{0},pm2dgl".format(org_point) + "~" + "{0},pm2rdl".format(toponym_longitude + ',' + toponym_lattitude)
 
 }
 
